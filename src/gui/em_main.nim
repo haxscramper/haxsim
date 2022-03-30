e96ea21ef5fa36f15c237c77cb5d71ed8605b7b9
haxscramper <haxscramper@gmail.com>
2022-03-30 15:15:12 +0300

    remove use of ~{.emit.}~ from imgui example

--- src/gui/em_main.nim
+++ src/gui/em_main.nim
@@ -1,30 +1,11 @@
-# Copyright 2018, NimGL contributors.
-
 import nimgl/imgui
-# , nimgl/imgui/[impl_opengl, impl_glfw]
-# import nimgl/[opengl, glfw]
 import hmisc/core/all
 import em_sokol
-{.emit: """/*INCLUDESECTION*/
-#include "sokol_app.h"
-#include "sokol_gfx.h"
-#include "sokol_glue.h"
-#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
-#include "cimgui.h"
-#include "sokol_imgui.h"
-#warning "Test warning message"
 
-static struct {
-    uint64_t laptime;
-    sg_pass_action pass_action;
-} state;
-""".}
-
-# var w: GLFWWindow
-# var show_demo: bool = true
-# var somefloat: float32 = 0.0f
-# var counter: int32 = 0
-# var context: ptr ImGuiContext
+var state: tuple[
+  passAction: SgPassAction,
+  laptime: uint64
+]
 
 template printedTrace*(body: untyped) =
   try:
@@ -34,95 +15,47 @@ template printedTrace*(body: untyped) =
     writeStackTrace()
     raise
 
-# var state
-
-# var desc: SgDesc
-# var sim: SimGuiDesec
-
 proc initImpl*(): void {.cdecl.} =
   startHax()
 
   printedTrace():
-    {.emit: """
-    sg_setup(&(sg_desc){
-        .context = sapp_sgcontext()
-    });
-    stm_setup();
-    simgui_setup(&(simgui_desc_t){ 0 });
+    var desc = SgDesc(context: sapp_sgcontext())
+    sg_setup(addr desc)
+    stm_setup()
+    var tmp = SimGuiDesc(maxVertices: 0)
+    simgui_setup(addr tmp)
 
-    // initial clear color
-    state.pass_action = (sg_pass_action) {
-        .colors[0] = {
-            .action = SG_ACTION_CLEAR,
-            .val = { 0.0f, 0.5f, 1.0f, 1.0 }
-        }
-    };
-""".}
-    # sg_setup(DgDesc(context: sapp_sgcontext()))
-    # simgui_setup(SimGuiDesc())
+    state.pass_action = SgPassAction()
+    state.pass_action.colors[0] = SgColorAttachmentAction(
+      action: SG_ACTION_CLEAR,
+      val: [0.0, 0.5, 1.0, 1.0]
+    )
 
 proc loopImpl*() {.cdecl.} =
   printedTrace():
-    # let width = sappWidth()
-    # let height = sappHeight()
-    {.emit: """
-    const int width = sapp_width();
-    const int height = sapp_height();
-    double delta_time = stm_sec(stm_round_to_common_refresh_rate(stm_laptime(&state.laptime)));
-    simgui_new_frame(width, height, delta_time);
-""".}
+    let width = sappWidth()
+    let height = sappHeight()
+    let deltaTime = stm_sec(stm_round_to_common_refresh_rate(
+      stm_laptime(addr state.laptime)))
 
-#     {.emit: """
-#     igSetNextWindowPos((ImVec2){10,10}, ImGuiCond_Once, (ImVec2){0,0});
-#     igSetNextWindowSize((ImVec2){400, 100}, ImGuiCond_Once);
-#     igBegin("Hello Dear ImGui!", 0, ImGuiWindowFlags_None);
-#     // igColorEdit3("Background", &state.pass_action.colors[0].val[0], ImGuiColorEditFlags_None);
-#     // igEnd();
-# """.}
+    simgui_new_frame(width, height, delta_time)
+
 
     igSetNextWindowPos(ImVec2(x: 10, y: 10), ImGuiCond.Once, ImVec2(x: 0, y: 0))
     igSetNextWindowSize(ImVec2(x: 400, y: 100), ImGuiCond.Once)
-    igBegin("Test", nil, ImGuiWindowFlags.None)
-    # igColorEdit3("Background", &state.pass_action.colors[0].val[0], ImGuiColorEditFlags_None)
-    # /// "IG end":
+    igBegin("Test ??", nil, ImGuiWindowFlags.None)
     igEnd()
 
 
-    {.emit: """
-    sg_begin_default_pass(&state.pass_action, width, height);
-    simgui_render();
-    sg_end_pass();
-    sg_commit();
-""".}
-    # simgui_new_frame(SimguiFrameDesc(
-    #     width:      sapp_width(),
-    #     height:     sapp_height(),
-    #     delta_time: sapp_frame_duration(),
-    #     dpi_scale:  sapp_dpi_scale(),
-    # })
-
-    # igSetNextWindowPos((ImVec2){10, 10}, ImGuiCond_Once, (ImVec2){0, 0});
-    # igSetNextWindowSize((ImVec2){400, 100}, ImGuiCond_Once);
-    # igBegin("Hello Dear ImGui!", 0, ImGuiWindowFlags_None);
-    # igColorEdit3(
-    #     "Background",
-    #     &state.pass_action.colors[0].value.r,
-    #     ImGuiColorEditFlags_None);
-    # igEnd();
-
-    # sg_begin_default_pass(&state.pass_action, sapp_width(), sapp_height())
-    # simgui_render()
-    # sg_end_pass()
-    # sg_commit()
+    sg_begin_default_pass(addr state.pass_action, width, height)
+    simgui_render()
+    sg_end_pass()
+    sg_commit()
 
 proc closeImpl*() {.cdecl.} =
   printedTrace():
-    {.emit: """
-    simgui_shutdown();
-    sg_shutdown();
-""".}
-    # simgui_shutdown()
-    # sg_shutdown()
+    simgui_shutdown()
+    sg_shutdown()
 
 proc eventImpl*(ev: ptr SappEvent) {.cdecl.} =
   printedTrace():
@@ -137,6 +70,3 @@ proc main(argc: cint, argv: ptr UncheckedArray[cstring]): SappDesc {.exportc: "s
     eventCb: eventImpl,
     window_title: "Test"
   )
-
-
-# main()
--- src/gui/em_sokol.nim
+++ src/gui/em_sokol.nim
@@ -10,26 +10,48 @@ type
     event_cb*: proc(ev: ptr SappEvent) {.cdecl.}
     window_title*: cstring
 
-  SimGuiDesc* {.importc: "struct simgui_desc", st.} = object
+  SimGuiDesc* {.importc: "simgui_desc_t", st.} = object
+    max_vertices*: cint
 
   SgContextDesc* {.importc: "sg_context_desc", st.} = object
 
   SappEvent* {.importc: "sapp_event", st.} = object
 
-  SgDesc* {.importc: "", st.} = object
+  SgDesc* {.importc: "sg_desc", st.} = object
     context*: SgContextDesc
+
+  SgAction* {.importc: "sg_action", st.} = enum
+    SG_ACTION_CLEAR = 1
+
+  SgColorAttachmentAction* {.importc: "sg_color_attachment_action", st.} = object
+    val*: array[4, float]
+    action*: SgAction
+
+  SgPassAction* {.importc: "sg_pass_action", st.} = object
+    colors*: array[4, SgColorAttachmentAction]
 
   # SappPassAction* {.importc: "struct sapp_pass_action", st.} = object
 
 proc sapp_sgcontext*(): SgContextDesc {.pri.}
-proc sg_commit*() {.pri.}
-proc sg_end_pass*() {.pri.}
-proc simgui_render*() {.pri.}
-proc sg_shutdown*() {.pri.}
-proc simgui_shutdown*() {.pri.}
-
 proc sapp_width*(): cint {.pri.}
 proc sapp_height*(): cint {.pri.}
 proc sapp_frame_duration*(): cint {.pri.}
 proc sapp_dpi_scale*(): cint {.pri.}
+
+proc sg_begin_default_pass*(act: ptr SgPassAction, w: cint, h: cint) {.pri.}
+proc sg_commit*() {.pri.}
+proc sg_end_pass*() {.pri.}
+proc sg_shutdown*() {.pri.}
+proc sg_setup*(desc: ptr SgDesc) {.pri.}
+
+proc simgui_setup*(desc: ptr SimguiDesc) {.pri.}
+proc simgui_render*() {.pri.}
+proc simgui_shutdown*() {.pri.}
+proc simgui_new_frame*(width, height: cint, delta: float) {.pri.}
 proc simgui_handle_event*(ev: ptr SappEvent) {.pri.}
+
+
+proc stm_setup*() {.pri.}
+proc stm_sec*(f: float): float {.pri.}
+proc stm_round_to_common_refresh_rate*(a: float): float {.pri.}
+proc stm_laptime*(prev: ptr uint64): float {.pri.}
